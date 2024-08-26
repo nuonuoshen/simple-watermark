adfaa0928133a9f41da76231bc620da6a35b9af0
nuo <2028911220@qq.com>
2024-08-26 17:34:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyWatermark from '@/demo/myWatermark.vue'
+</script>
+
+<template>
+  <MyWatermark />
+</template>
+
+<style scoped>
+</style>
---
+++ src/demo/myWatermark.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import watermark from '@/components/watermark/index.vue'
+</script>
+
+<template>
+  <watermark>
+    <div>
+    <p>
+    这是水印组件的插槽中的内容。This is the content in the slot of the watermark component.
+    这是水印组件的插槽中的内容。This is the content in the slot of the watermark component.
+    这是水印组件的插槽中的内容。This is the content in the slot of the watermark component.
+    这是水印组件的插槽中的内容。This is the content in the slot of the watermark component.
+    </p>
+  </div>
+  </watermark>
+  
+</template>
+
+<style scoped>
+</style>
